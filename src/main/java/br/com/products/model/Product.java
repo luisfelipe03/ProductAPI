1496f1195bad2c1f91dab6d21846e833f170e3bb
Luis Felipe <llipeandrade34@gmail.com>
2023-12-02 13:10:11 -0300

    implementando a entidade product

--- src/main/java/br/com/products/model/Product.java
+++ src/main/java/br/com/products/model/Product.java
@@ -1,5 +1,10 @@
 package br.com.products.model;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,11 +13,67 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "product")
-public class Product {
+public class Product implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	
+	private UUID id;
+	private String name;
+	private BigDecimal value;
+
+	public Product(UUID id, String name, BigDecimal value) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.value = value;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", value=" + value + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, value);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name) && Objects.equals(value, other.value);
+	}
+
 }
